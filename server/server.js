e2078d59d630fbdf6568bbe063d5cb677556dd54
Mehul Gopal Nyachhyon <aventmehul@gmail.com>
2025-07-30 21:53:30 +0545

    Feature: Added required basic files

--- server/server.js
+++ server/server.js
@@ -3,16 +3,16 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
 
-process.on("warning", (warning) => {
-  if (
-    warning.name === "DeprecationWarning" &&
-    warning.message.includes("MONGODB DRIVER")
-  ) {
-    // Skip MongoDB DRIVER warnings
-  } else {
-    console.warn(warning);
-  }
-});
+// process.on("warning", (warning) => {
+//   if (
+//     warning.name === "DeprecationWarning" &&
+//     warning.message.includes("MONGODB DRIVER")
+//   ) {
+//     // Skip MongoDB DRIVER warnings
+//   } else {
+//     console.warn(warning);
+//   }
+// });
 
 dotenv.config();
 connectDB();
@@ -24,10 +24,13 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", require("./routes/auth.routes"));
 
-// Default route
-app.get("/", (req, res) => {
-  res.send(" API is running...");
-});
+app.use("/api/rooms", require("./routes/room.routes"));
+app.use("/api/bookings", require("./routes/booking.routes"));
+
+// // Default route
+// app.get("/", (req, res) => {
+//   res.send(" API is running...");
+// });
 
 // Start server
 const PORT = process.env.PORT || 5000;
